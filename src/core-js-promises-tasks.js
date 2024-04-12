bed7f4cde188ecf2b5efd8817ba9085ce5c65f30
KirillGenin <332211op@gmail.com>
2024-04-12 16:44:56 +0300

    feat: implement function queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -136,8 +136,18 @@ async function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  let count = 0;
+
+  return new Promise((resolve) => {
+    const results = promises.map((promise, idx) =>
+      promise.then((value) => {
+        results[idx] = value;
+        count += 1;
+        if (count === promises.length) resolve(results.join(''));
+      })
+    );
+  });
 }
 
 module.exports = {
